146728aceacb348f8c5b621866c5527c22a1e344
2144019 <madduri.saipraneethreddy2@cognizant.com>
2025-01-22 15:39:01 +0530

    authentication permit for open api urls

--- api-gateway/src/main/java/com/learn/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/learn/microservices/gateway/config/SecurityConfig.java
@@ -10,9 +10,14 @@ import java.net.http.HttpRequest;
 
 @Configuration
 public class SecurityConfig {
+
+    private final String[] freeResourceUrls = {"/api-docs", "/swagger-ui.html"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+        return httpSecurity.authorizeHttpRequests(request -> request
+                        .requestMatchers(freeResourceUrls).permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
